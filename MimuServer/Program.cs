a5eee3aa9bc010b6572b8f6dec47a959ef225b98
itsveiliss <sashas2811@gmail.com>
2026-09-22 21:55:34 +0300

    Fixing the server-side issue that kills the client connection after N seconds

--- MimuServer/Program.cs
+++ MimuServer/Program.cs
@@ -85,7 +85,7 @@ async Task HandleClientAsync(TcpClient client, MessagesRepository messagesReposi
 
         try
         {
-            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(20));
             var received = await limitedRead.ReadLineLimitedAsync(reader, cts.Token);
             if (received == null) break;
             if (string.IsNullOrWhiteSpace(received))
